038474b51afb158b17942b7bda557b5439f058aa
linh66888 <hoangthaolinh1101@gmail.com>
2024-08-06 15:55:05 +0700

    initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x4EA5c96F77De86Bf5b9b3833a5ff5FDebCa23282'
-export const ProxyAdmin = '0xCC227e968ADf385761037A37964B3e8B63ab6933'
-export const SWTRImplementation = '0xafE0EB15C8a5e7E9feaC596227f7957f699e5e22'
+export const SWTRProxy = '0xDedC006e9AC0C62a4354fac24d25f6B218e9ECEc'
+export const ProxyAdmin = '0x4848a154D504aC36034F27B0CeCe1B0D758F2129'
+export const SWTRImplementation = '0x769f4167D8945b874d440859Ab5792Df080450E8'
     
