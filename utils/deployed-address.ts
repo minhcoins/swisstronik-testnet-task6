f53c95a84b445bdbd1d1e23269f3a017ef7b2ee0
linhucash <johnbrownwave@gmail.com>
2024-08-08 21:31:43 +0700

    initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x86eE1d30f1E52072c1C80fdEac136a1D92066831'
-export const ProxyAdmin = '0x9BC5228aE959CAbdbb5baCEfd137B09A4b7b3071'
-export const SWTRImplementation = '0xed260946f5A3ADc832f0ff7650C142239c19C958'
+export const SWTRProxy = '0x6507400Ab382fbb8E4bD1c250db5c6c1CA4B0FAB'
+export const ProxyAdmin = '0x2b887C2A1Ad86686a1d0B514d5cC95fC5685b5FD'
+export const SWTRImplementation = '0x9d8566DE2A3ba4BC75271332A6FD3e7cfC255ef8'
     
